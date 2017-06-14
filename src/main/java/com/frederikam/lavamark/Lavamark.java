df0707e7fa4db814913caa22223d32848eae6205
Frederikam <frogkr@gmail.com>
2017-06-14 16:28:37 +0200

    Configure audio player manager

--- src/main/java/com/frederikam/lavamark/Lavamark.java
+++ src/main/java/com/frederikam/lavamark/Lavamark.java
@@ -1,7 +1,20 @@
 package com.frederikam.lavamark;
 
-/**
- * Created by frederik on 14-Jun-17.
- */
+import com.sedmelluq.discord.lavaplayer.player.AudioConfiguration;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+
 public class Lavamark {
+
+    private static final AudioPlayerManager PLAYER_MANAGER = new DefaultAudioPlayerManager();
+
+    public static void main(String[] args) {
+        /* Set up the player manager */
+        PLAYER_MANAGER.enableGcMonitoring();
+        PLAYER_MANAGER.setItemLoaderThreadPoolSize(100);
+        PLAYER_MANAGER.getConfiguration().setResamplingQuality(AudioConfiguration.ResamplingQuality.LOW);
+        AudioSourceManagers.registerRemoteSources(PLAYER_MANAGER);
+    }
+
 }
