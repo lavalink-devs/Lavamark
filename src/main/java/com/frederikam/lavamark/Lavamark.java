1c0bdb5b4dbd212766911c0f0cf9a6f1f4ed83b3
devoxin <lukel9914@gmail.com>
2024-03-26 11:40:54 +0000

    Update LP, add cmdline options

--- src/main/java/com/frederikam/lavamark/Lavamark.java
+++ src/main/java/com/frederikam/lavamark/Lavamark.java
@@ -28,10 +28,16 @@ import com.sedmelluq.discord.lavaplayer.player.AudioConfiguration;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import com.sedmelluq.lava.extensions.youtuberotator.YoutubeIpRotatorSetup;
+import com.sedmelluq.lava.extensions.youtuberotator.planner.RotatingNanoIpRoutePlanner;
+import com.sedmelluq.lava.extensions.youtuberotator.tools.ip.Ipv6Block;
+import org.apache.commons.cli.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -40,7 +46,6 @@ public class Lavamark {
     private static final Logger log = LoggerFactory.getLogger(Lavamark.class);
 
     static final AudioPlayerManager PLAYER_MANAGER = new DefaultAudioPlayerManager();
-    private static final String DEFAULT_PLAYLIST = "https://www.youtube.com/watch?v=7v154aLVo70&list=LLqqLoSLryroL7b7TAL8gfhQ&index=22";
     private static final String DEFAULT_OPUS = "https://www.youtube.com/watch?v=M_36UBLkni8";
     private static final long INTERVAL = 2 * 1000;
     private static final long STEP_SIZE = 20;
@@ -57,20 +62,58 @@ public class Lavamark {
         PLAYER_MANAGER.getConfiguration().setResamplingQuality(AudioConfiguration.ResamplingQuality.LOW);
         AudioSourceManagers.registerRemoteSources(PLAYER_MANAGER);
 
-        log.info("Loading AudioTracks");
+        String jarPath = Lavamark.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        String jarName = jarPath.substring(jarPath.lastIndexOf("/") + 1);
 
-        String identifier;
-        if (args.length >= 1 && args[0].equals("opus")) {
-            identifier = DEFAULT_OPUS;
-        } else {
-            identifier = DEFAULT_PLAYLIST;
+        Options options = new Options()
+            .addOption("b", "block", true, "The IPv6 block to use for rotation. This must be specified as CIDR notation.")
+            .addOption("s", "step", true, "The number of players to spawn after a fixed interval. Be careful when using large values.")
+            .addOption("i", "identifier", true, "The audio identifier to use for the test. Must be a URL pointing to a supported audio source.")
+            .addOption("h", "help", false, "Displays the supported command line arguments.");
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+
+        CommandLine parsed;
+
+        try {
+            parsed = parser.parse(options, args);
+        } catch (ParseException parseException) {
+            formatter.printHelp("java -jar " + jarName, options);
+            return;
         }
+
+        if (parsed.hasOption("help")) {
+            formatter.printHelp("java -jar " + jarName, options);
+            return;
+        }
+
+        if (parsed.hasOption("block")) {
+            String ipBlock = parsed.getOptionValue("block");
+
+            YoutubeAudioSourceManager ytasm = PLAYER_MANAGER.source(YoutubeAudioSourceManager.class);
+            RotatingNanoIpRoutePlanner planner = new RotatingNanoIpRoutePlanner(Collections.singletonList(new Ipv6Block(ipBlock)));
+            new YoutubeIpRotatorSetup(planner).forSource(ytasm).setup();
+
+            log.info("IP rotation configured.");
+        }
+
+        String identifier = parsed.getOptionValue("identifier", DEFAULT_OPUS);
+
+        log.info("Loading AudioTracks from identifier {}", identifier);
 
         tracks = new PlaylistLoader().loadTracksSync(identifier);
         log.info(tracks.size() + " tracks loaded. Beginning benchmark...");
 
+        long stepSize = STEP_SIZE;
+
+        if (parsed.hasOption("step")) {
+            stepSize = Long.getLong(parsed.getOptionValue("step"));
+            log.info("Step set to {} players every {} milliseconds.", stepSize, INTERVAL);
+        }
+
         try {
-            doLoop();
+            doLoop(stepSize);
         } catch (Exception e) {
             log.error("Benchmark ended due to exception!");
             throw new RuntimeException(e);
@@ -79,10 +122,10 @@ public class Lavamark {
         System.exit(0);
     }
 
-    private static void doLoop() throws InterruptedException {
+    private static void doLoop(long stepSize) throws InterruptedException {
         //noinspection InfiniteLoopStatement
         while (true) {
-            spawnPlayers();
+            spawnPlayers(stepSize);
 
             AudioConsumer.Results results = AudioConsumer.getResults();
             log.info("Players: " + players.size() + ", Null frames: " + results.getLossPercentString());
@@ -98,8 +141,8 @@ public class Lavamark {
         }
     }
 
-    private static void spawnPlayers() {
-        for (int i = 0; i < STEP_SIZE; i++) {
+    private static void spawnPlayers(long stepSize) {
+        for (int i = 0; i < stepSize; i++) {
             players.add(new Player());
         }
     }
